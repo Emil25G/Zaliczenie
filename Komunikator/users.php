06d5c6fad0938454196632d454717825b95a34e3
Emil Gielek <emil.gielek@gmail.com>
2023-03-16 23:55:41 +0100

    Dodanie users.php, zmiana stylów i itp

---
+++ Komunikator/users.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!-- Emil Gielek & Szymon Gładki  CHAT APP-->
+<html lang="en">
+    <head>
+        <meta charset="UFT-8">
+        <meta name="vierwport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Komunikator szkolny</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.2/css/all.min.css">
+    </head> 
+    <body> 
+        <div class="wrapper">
+            <section class="users">
+                <header>
+                    <div class="content"> 
+                        <img src="img.jpg" alt="">
+                        <div class="details">
+                            <span> WSB - K_27 </span>
+                            <p> Aktywny teraz </p>
+                        </div>
+                    </div>
+                    <a href="#" class="logout">Wyloguj</a>    
+                </header>  
+                <div class="search">
+                    <span class="text">Wybierz osobę aby rozpocząć czat</span>
+                    <input type="text" placeholder="Wyszukaj osobę...">
+                    <button> <i class="fas fa-search"></i></button>
+                </div>  
+            </section>
+        </div>
+    </body>
+
+</html>
