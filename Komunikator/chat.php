4b082a9939ee06c068910435a9abd1f119772e7d
Emil Gielek <emil.gielek@gmail.com>
2023-03-17 18:59:51 +0100

    Naprawiłem arkusz stylów z font awesome

--- Komunikator/chat.php
+++ Komunikator/chat.php
@@ -7,7 +7,7 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Komunikator szkolny</title>
         <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.2/css/all.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
     </head> 
     <body> 
         <div class="wrapper">
